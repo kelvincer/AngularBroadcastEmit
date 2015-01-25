44ef7b9577e3abdf4062f6fb54c96093aa20a208
Kelvin Cervan <kcervanr@gmail.com>
2015-01-25 11:17:33 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kelvin on 1/25/2015.
+ */
+/*From de book AngularJS novice to ninja
+* Page: 66
+* */
+
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+    'myApp.controllers','ngRoute'
+]);
